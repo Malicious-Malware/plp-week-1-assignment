67afa07a7f4d027fb4e605396e470604b9bba67d
Malicious-Malware <vostman83@gmail.com>
2024-04-14 11:50:50 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print("Hello Good world")
+Site_Name = 'PLP'
+
+print (Site_Name)
+
+Site_Name = 'Dundanet'
+print(Site_Name)
+
+
+a,b,c = 27,83,"lol"
+
+print(a)
+
+print(b)
+
+print(c)
+
+num1 = 6
+
+print(num1, 'is of type', type(num1))
+
+num2 = 5.6
+print(num2, 'is of type', type(num2))
+
+num3 = 45+3j
+print(num3, 'is of type', type(num3))
