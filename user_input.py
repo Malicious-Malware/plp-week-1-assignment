4c2b3b1685dfb4071053f3b979162b9f433e66ab
Malicious-Malware <vostman83@gmail.com>
2024-04-14 11:45:31 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input('Please enter your name: ')
+
+age = input('Please enter your age: ')
+
+location = input('Where are you located? ')
+
+print('Hello ' + name + ', you are ' + age + ' years old and live in ' + location.)
